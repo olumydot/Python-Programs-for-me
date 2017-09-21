1050b9fc96b0958e41ce80c045c0715a30d26bbf
[Olumide] <[olutaiwo.oni@gmail.com]>
2017-09-21 05:39:33 -0400

    just some very minor fixes

--- Deola Game decision.py
+++ Deola Game decision.py
@@ -1,32 +1,32 @@
 while True:  # while this truth remains this code will run. It will loop infinitely unless I break it at some point.
-    your_answers = ["Yes", "No", "Maybe", "Maybe not"]
+
+    your_answers = ["yes", "no", "maybe", "maybe not"]
     print("Welcome to this fun game by Olu T. Oni.\n")
     print("\nPlease enter: Yes, No, Maybe or Maybe not. Note that each start with a capital letter")
     print("\nThis application is strictly for Deola. If you are not Deola you will be insulted")
+
     name = input("\nwhat is your name?  ")
+
     if name != "Deola":
-        print("\nDear {}, This application is not for you so fuck off".format(name))
+        print("\nDear {}, This application is not for you so buzz off".format(name))
         break
     else:
-
         ans = input(name + " " + "Do you want to take Erhabhor seriously?: ")
+        ans = ans.lower()
+
     if ans not in your_answers:  # if here imposes some condition. It it were 'for' then it will look in an iterable list. for x in b <= thats how its used
-        print("\n{} the answers you gave is wrong. Try again!".format(
+        print("\n{} Be smart. You could have typed \"No\" or something like that! Try again".format(
             name))  # if answer is unacceptable it prints this and runs again
 
-    if ans != "No":
+    elif ans != "no":
         print("\nWrong decision {}. I don't trust Erhabhor and so shouldn't you".format(
             name))  # if not no it prints this and goes to the beginning again
+        quit()
 
     else:  # if it is found in your_answers or not no, then it prints the below
         print(
             "correct decision {}. you have a good head on your shoulders. The world is tough but you are tougher".format(
                 name))
-        print("\nHave a blissful day or night {}. ".format(name))
-        new_input = input("Please enter \"quit\" to respectfully quit this awesome game Adeola\n")
-        if new_input == "Quit" or "quit":
-            break
-        else:
-
-            break  # unlike before when it goes to the beginning of the loop The break statement can be used in both while and for loops.
+        print("Have a blissful day or night {}. ".format(name))
+        input("Please enter to quit awesome game Adeola\n")
 
